d8e6799ecfcf8746194bb6ac492f1cbd2d8b61c0
xaquaaa <aquakuttan@gmail.com>
2026-09-23 15:43:18 -0400

    TASK1:Emotion detector func

---
+++ emotion_detection.py
@@ -0,0 +1,17 @@
+import requests
+
+#FUNCTION emotion_detector to run emotion detection
+def emotion_detector(text_to_analyze):
+    # URL of the emotion detection service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    #custom header for emotion detection service
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    #request paylod in desired format
+    myobj = { "raw_document": { "text": text_to_analyze } }
+
+    #sending a post request for emotion detection service
+    response = requests.post(url, json=myobj, headers=header)
+
+    return response.text
