d69d5c8f40662f3b7a35fdb1ecfa52e5b1207219
xaquaaa <aquakuttan@gmail.com>
2026-09-23 16:26:37 -0400

    TASK2:emotion_detector formatting output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 #FUNCTION emotion_detector to run emotion detection
 def emotion_detector(text_to_analyze):
@@ -14,4 +15,35 @@ def emotion_detector(text_to_analyze):
     #sending a post request for emotion detection service
     response = requests.post(url, json=myobj, headers=header)
 
-    return response.text
+    #format the response to dict using json
+    formatted_response = json.loads(response.text)
+
+    #extracting required emotions
+    anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+    disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+    fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+    joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+    sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+
+    emotions = {
+        "anger":anger_score,
+        "disgust":disgust_score,
+        "fear":fear_score,
+        "joy":joy_score,
+        "sadness":sadness_score
+    }
+
+    #find dominant emotion
+    dominant_emotion = max(emotions, key = emotions.get)
+
+    #formatted output response
+    formatted_emotions = {
+        "anger":anger_score,
+        "disgust":disgust_score,
+        "fear":fear_score,
+        "joy":joy_score,
+        "sadness":sadness_score,
+        "dominant_emotion": dominant_emotion       
+    }
+
+    return formatted_emotions
